ce6d8d79ec50b1660c7f0f0cba17b6357882532b
Mattia Saponaro <mattia.saponaro2@studio.unibo.it>
2026-04-09 15:49:02 +0200

    Added HealthHttpHandler for GET /health

--- hotel-node/src/main/java/it/unibo/distributedbooking/hotelnode/api/HealthHttpHandler.java
+++ hotel-node/src/main/java/it/unibo/distributedbooking/hotelnode/api/HealthHttpHandler.java
@@ -2,40 +2,36 @@ package it.unibo.distributedbooking.hotelnode.api;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import it.unibo.distributedbooking.common.model.BookingRequest;
-import it.unibo.distributedbooking.common.model.BookingResponse;
-import it.unibo.distributedbooking.common.service.BookingService;
+import it.unibo.distributedbooking.common.model.HealthResponse;
 import it.unibo.distributedbooking.hotelnode.util.JsonUtil;
 
 import java.io.IOException;
 import java.io.OutputStream;
 
-public class BookingHttpHandler implements HttpHandler {
+public class HealthHttpHandler implements HttpHandler {
 
-    private final BookingService bookingService;
+    private final String hotelId;
 
-    public BookingHttpHandler(BookingService bookingService) {
-        this.bookingService = bookingService;
+    public HealthHttpHandler(final String hotelId) {
+        this.hotelId = hotelId;
     }
 
     @Override
-    public void handle(final HttpExchange exchange) throws IOException {
-        if (!"POST".equalsIgnoreCase(exchange.getRequestMethod())) {
+    public void handle(HttpExchange exchange) throws IOException {
+        if (!"GET".equalsIgnoreCase(exchange.getRequestMethod())) {
             exchange.sendResponseHeaders(405, -1);
             return;
         }
         try {
-            BookingRequest request = JsonUtil.fromJson(exchange.getRequestBody(), BookingRequest.class);
-            BookingResponse response = this.bookingService.createBooking(request);
+            HealthResponse response = new HealthResponse(this.hotelId, "UP");
             byte[] responseBytes = JsonUtil.toJsonBytes(response);
             exchange.getResponseHeaders().add("Content-Type", "application/json");
             exchange.sendResponseHeaders(200, responseBytes.length);
             try (OutputStream os = exchange.getResponseBody()) {
                 os.write(responseBytes);
             }
-        } catch (Exception e) {
-            byte[] errorBytes = JsonUtil.toJsonBytes(new BookingResponse(
-                    null, false, "Internal server error: " + e.getMessage(), null));
+        } catch (Exception e){
+            byte[] errorBytes = JsonUtil.toJsonBytes(new HealthResponse(hotelId, "DOWN"));
             exchange.getResponseHeaders().add("Content-Type", "application/json");
             exchange.sendResponseHeaders(500, errorBytes.length);
             try (OutputStream os = exchange.getResponseBody()) {
